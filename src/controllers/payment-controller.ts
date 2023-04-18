3596733d51f6e3fd69ad838a633be35e937ca04a
Isabella Lopes <icmlopes@gmail.com>
2023-04-18 14:13:17 -0300

    feat: payment route architecture
    Signed-off-by: Isabella Lopes <icmlopes@gmail.com>

---
+++ src/controllers/payment-controller.ts
@@ -0,0 +1,11 @@
+import { AuthenticatedRequest } from "@/middlewares";
+import { Response } from "express";
+
+
+export async function createPayment(req: AuthenticatedRequest, res: Response){
+
+}
+
+export async function getPayment(req: AuthenticatedRequest, res: Response){
+    
+}
---
+++ src/routers/payments-router.ts
@@ -0,0 +1,9 @@
+import { authenticateToken } from '@/middlewares';
+import { Router } from 'express';
+
+const paymentsRouter = Router();
+
+paymentsRouter.all('/*', authenticateToken);
+paymentsRouter.post('')
+
+export  {paymentsRouter}
